7a55065dc6680cae291fc4346b6fc37b3341dacc
Wzy-23 <nkc20246080@st.denpa.jp>
2026-05-28 12:22:34 +0900

    Update TaskController.php

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+}
