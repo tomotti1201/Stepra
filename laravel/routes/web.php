72dfe3dc3a5356da75e42a595d2495f9932c2f45
Satoukk <gotokouzou312@icloud.com>
2026-06-02 13:35:21 +0900

    scheduleAPI完成

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,6 +19,10 @@ Route::get('/newuser',function(){
     return view('newuser');
 });
 
+Route::get('/schedule', function () {
+    return response()->file(resource_path('views/frontend/schedule.html'));
+});
+
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::get('/user/tasks', [TaskController::class, 'userTask']);
 Route::post('/tasks', [TaskController::class, 'store']);
