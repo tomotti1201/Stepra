88c428ba53ef2455e319b0a351b2ec0921f9f4ac
Wzy-23 <nkc20246080@st.denpa.jp>
2026-06-05 11:56:00 +0900

    グループタスクモデル完成

--- laravel/app/Models/Group_task.php
+++ laravel/app/Models/Group_task.php
@@ -5,18 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Group extends Model
+class Group_task extends Model
 {
     use HasFactory;
 
-    protected $table = 'group_task';
+    protected $table = 'group_tasks';
 
     protected $fillable = [
-   'group_id',
+        'group_id',
         'title',
         'content',
         'created_by',
     ];
-    
+
     public $timestamps = false;
 }
