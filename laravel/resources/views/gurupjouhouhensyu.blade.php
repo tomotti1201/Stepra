fa7852dd8be821d020e39734796dd2c3dfccf383
Satoukk <gotokouzou312@icloud.com>
2026-07-10 14:22:12 +0900

    招待コード修正

--- laravel/resources/views/gurupjouhouhensyu.blade.php
+++ laravel/resources/views/gurupjouhouhensyu.blade.php
@@ -128,6 +128,7 @@
   </div>
 <x-menubar />
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     const groupId = @json($group->id);
     const currentUserId = localStorage.getItem('user_id');
